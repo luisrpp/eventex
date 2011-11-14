dc6694ed25bbc2208101e84672dd100fd468dfec
Luis Roberto Pereira de Paula <luisrpp@gmail.com>
2011-11-14 03:27:34 -0200

    Updating EventeX

--- src/core/views.py
+++ src/core/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 def homepage(request):
-	context = RequestContext(request)
-	return render_to_response('index.html', context)
+    context = RequestContext(request)
+    return render_to_response('index.html', context)

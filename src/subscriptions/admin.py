85561f3c30cd23eaf391b47a663c062a54572683
Luis Roberto Pereira de Paula <luisrpp@gmail.com>
2011-11-27 14:52:48 -0200

    Add paid field

--- src/subscriptions/admin.py
+++ src/subscriptions/admin.py
@@ -1,17 +1,31 @@
+# -*- coding: utf-8 -*-
 import datetime
 from django.contrib import admin
 from subscriptions.models import Subscription
+from django.utils.translation import ugettext as _
+from django.utils.translation import ungettext
 
 class SubscriptionAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'phone', 'created_at', 'subscribed_today')
+    list_display = ('name', 'email', 'phone', 'created_at', 'subscribed_today', 'paid')
     date_hierarchy = 'created_at'
     search_fields = ('name', 'cpf', 'email', 'phone', 'created_at')
-    list_filter = ['created_at']
+    list_filter = ('paid', )
+    actions = ['mark_as_paid']
 
     def subscribed_today(self, obj):
         return obj.created_at.date() == datetime.date.today()
-
+        
     subscribed_today.short_description = u'Inscrito hoje?'
     subscribed_today.boolean = True
+
+    def mark_as_paid(self, request, queryset):
+        count = queryset.update(paid=True)
+        msg = ungettext(
+            u'%(count)d inscrição foi marcada como paga.',
+            u'%(count)d inscrições foram marcadas como pagas.',
+            count
+        ) % {'count': count}
+        self.message_user(request, msg)
+    mark_as_paid.short_description = _(u"Marcar como pagas")
 
 admin.site.register(Subscription, SubscriptionAdmin)
--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -6,6 +6,7 @@ class Subscription(models.Model):
     cpf = models.CharField('CPF', max_length=11, unique=True)
     email = models.EmailField('E-mail', unique=True)
     phone = models.CharField('Telefone', max_length=20, blank=True)
+    paid = models.BooleanField()
     created_at = models.DateTimeField('Criado em', auto_now_add=True)
 
     def __unicode__(self):

81a7c2918cc67d3234f5286dd89c97bf56514d90
Luis Roberto Pereira de Paula <luisrpp@gmail.com>
2011-11-29 00:04:32 -0200

    Adding Custom View

--- src/settings.py
+++ src/settings.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Django settings for src project.
 import os
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
--- src/subscriptions/admin.py
+++ src/subscriptions/admin.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 from subscriptions.models import Subscription
 from django.utils.translation import ugettext as _
 from django.utils.translation import ungettext
+from django.conf.urls.defaults import patterns, url
+from django.http import HttpResponse
 
 class SubscriptionAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'phone', 'created_at', 'subscribed_today', 'paid')
@@ -14,7 +16,7 @@ class SubscriptionAdmin(admin.ModelAdmin):
 
     def subscribed_today(self, obj):
         return obj.created_at.date() == datetime.date.today()
-        
+
     subscribed_today.short_description = u'Inscrito hoje?'
     subscribed_today.boolean = True
 
@@ -27,5 +29,28 @@ class SubscriptionAdmin(admin.ModelAdmin):
         ) % {'count': count}
         self.message_user(request, msg)
     mark_as_paid.short_description = _(u"Marcar como pagas")
+
+    def export_subscriptions(self, request):
+        subscriptions = self.model.objects.all()
+        rows = [','.join([s.name, s.email]) for s in subscriptions]
+        
+        response = HttpResponse('\r\n'.join(rows))
+        response.mimetype = 'text/csv'
+        response['Content-Disposition'] = 'attachment; filename=inscricoes.csv'
+        
+        return response
+    
+    def get_urls(self):
+        original_urls = super(SubscriptionAdmin, self).get_urls()
+        extra_urls = patterns('',
+            # Envolvemos nossa view em 'admin view' por que ela faz o
+            # controle de permissões e cache automaticamente para nós.
+            url(r'exportar-inscricoes/$',
+                self.admin_site.admin_view(self.export_subscriptions),
+                name='export_subscriptions')
+        )
+        # A ordem é importante. As URLs originais do admin são muito permissivas
+        # e acabam sendo encontradas antes da nossa se elas estiverem na frente.
+        return extra_urls + original_urls
 
 admin.site.register(Subscription, SubscriptionAdmin)
